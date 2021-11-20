ed819bcbd322de0ef1db9cd3f7b0d9178f3e53f1
Meauma13 <86557478+Meauma13@users.noreply.github.com>
2021-11-20 12:22:58 +0100

    Used the Include Directive to simplify development

---
+++ header.php
@@ -0,0 +1,38 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">Group2 Capstone Project</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="home.php">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="index.php">All Conversions</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="area.php">Area</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="volume.php">Volume</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="lengthconvert.php">Length</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="temperature.php">Temparature</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="speed.php">Speed</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="weight.php">Weight</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="time.php">Time</a>
+            </li>            
+            </ul>
+        </div>
+    </nav>
--- home.php
+++ home.php
@@ -8,44 +8,8 @@
     <link href="resources/bootstrap.min.css" rel="stylesheet" >
     <link rel="stylesheet" href="index.css">
 
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">Group2 Capstone Project</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-        </button>
+    <?php include 'header.php'; ?>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-                <a class="nav-link" href="home.php">Home</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="index.php">All Conversions</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="area.php">Area</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="volume.php">Volume</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="lengthconvert.php">Length</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="temperature.php">Temparature</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="speed.php">Speed</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="weight.php">Weight</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="time.php">Time</a>
-            </li>            
-            </ul>
-        </div>
-    </nav>
 </head>
 
 <body>
@@ -62,5 +26,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="resources/bootstrap.min.js"></script>
 
+    <?php include "footer.php"; ?>
+    
 </body>
 </html>
